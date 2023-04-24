f48eb2c6e45acef939e09c2f46f5e3f2171229ca
huy36 <huy36@gmail.com>
2023-04-24 15:49:43 +0700

    v17

--- src/App.js
+++ src/App.js
@@ -8,6 +8,9 @@ import {
   Album,
   WeekRank,
   ChartZing,
+  Search,
+  SearchSongs,
+  SearchAll,
 } from './containers/public'
 import { Routes, Route } from 'react-router-dom'
 import path from './ultis/path'
@@ -33,6 +36,10 @@ function App() {
             <Route path={path.PLAYLIST__TITLE__PID} element={<Album />} />
             <Route path={path.WEEKRANK__TITLE__PID} element={<WeekRank />} />
             <Route path={path.ZING_CHART} element={<ChartZing />} />
+            <Route path={path.SEARCH} element={<Search />}>
+              <Route path={path.ALL} element={<SearchAll />} />
+              <Route path={path.SONG} element={<SearchSongs />} />
+            </Route>
 
             <Route path={path.STAR} element={<Home />} />
           </Route>
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,9 +1,19 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import icons from '../ultis/icons'
+import { apiSearch } from '../apis'
 
 const { BsSearch } = icons
 
 const Search = () => {
+  const [keyword, setKeyword] = useState('')
+
+  const handleSearch = async (e) => {
+    if (e.keyCode === 13) {
+      const response = await apiSearch(keyword)
+      console.log(response)
+    }
+  }
+
   return (
     <div className='w-full flex items-center'>
       <span className='h-10 pl-4 bg-[#2F2739] flex items-center justify-center rounded-l-[20px] text-gray-500'>
@@ -13,6 +23,9 @@ const Search = () => {
         type='text'
         className='outline-none bg-[#2F2739] px-4 py-2 w-full rounded-r-[20px] h-10 text-gray-500'
         placeholder='Nhập tên bài hát, nghệ sĩ hoặc MV...'
+        value={keyword}
+        onChange={(e) => setKeyword(e.target.value)}
+        onKeyUp={handleSearch}
       />
     </div>
   )
